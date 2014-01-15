c50f85029553383e86f5658faedabdd6fee94ac6
Serge Domkowski <sergedomk@gmail.com>
2014-01-14 20:58:40 -0800

    Added chardet and testing.

--- email_utils/encoding.py
+++ email_utils/encoding.py
@@ -5,11 +5,12 @@ Email encoding.
 from __future__ import unicode_literals
 
 import re
+import chardet
 from email.header import decode_header
 
 JOIN_CHAR = "".encode('utf-8')
 
-def get_decoded_header(text):
+def get_decoded_email_header(text):
     """Get the decoded value for the email header text passed in.
 
     Args:
@@ -25,8 +26,24 @@ def get_decoded_header(text):
     parts = decode_header(text)
     decoded_parts = []
     for part, charset in parts:
-        part_decoded = part.decode(charset or 'ascii')
-        decoded_parts.append(part_decoded.encode('utf-8'))
+        decoded_parts.append(decode_string_to_unicode(part, charset))
     return JOIN_CHAR.join(decoded_parts)
 
+def decode_string_to_unicode(text, charset=None):
+    """Get the unicode value of text using provided charset. If the charset
+    is invalid attempt to guess what it is.
 
+    Args:
+        text (string): The string we want to decode.
+        charset (string): The charset we think this is. Defaults to ascii.
+
+    Returns:
+        (unicode) The decoded unicode value.
+    """
+    try:
+        if isinstance(text, unicode):
+            return text
+        return text.decode(charset or 'ascii', 'strict')
+    except (UnicodeError, LookupError):
+        charset = chardet.detect(text)
+        return text.decode(charset['encoding'], 'replace')
--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -9,40 +9,55 @@ import unittest
 import email_utils.encoding
 
 class TestEncoding(unittest.TestCase):
-    def test_get_decoded_header(self):
-        test_headers = [
-            "=?iso-2022-jp?b?GyRCRW1CQE86GyhCIDxtb21vQHRhcm8ubmUuanA=?=",
-            "honyaku@googlegroups.com",
-            "\"=?UTF-8?Q?Igor_=C5=A0erko?=\"",
-            "=?UTF-8?B?5qGD5aSqLCDpg44=?= ",
-        ]
-        for header in test_headers:
-            decoded = email_utils.encoding.get_decoded_header(header)
-            print decoded
+    def test_get_decoded_email_header(self):
+        test_headers = {
+            "桃太郎 <momo@taro.ne.jp":
+                "=?iso-2022-jp?b?GyRCRW1CQE86GyhCIDxtb21vQHRhcm8ubmUuanA=?=",
+            "honyaku@googlegroups.com":
+                "honyaku@googlegroups.com",
+            "\"Igor Šerko\"": "\"=?UTF-8?Q?Igor_=C5=A0erko?=\"",
+            "桃太, 郎": "=?UTF-8?B?5qGD5aSqLCDpg44=?= ",
+            "桃太郎 <momo@taro.ne.jp>":
+                "=?iso-2022-jp?b?GyRCRW1CQE86GyhCID?=\t<momo@taro.ne.jp>"
+        }
+        for header, encoded in test_headers.iteritems():
+            decoded = email_utils.encoding.get_decoded_email_header(encoded)
+            self.assertEqual(header, decoded)
 
-    def test_get_decoded_header_subject(self):
+    def test_get_decoded_email_header_subject(self):
         """
         Test material grabbed from questions regarding decoding email subject
         headers that I found on forums and blogs such as stackoverflow.
         """
-        test_subjects = [
-            "[ 201105161048 ] GewSt:=?UTF-8?B?IFdlZ2ZhbGwgZGVyIFZvcmzDpHV" + \
-                "maWdrZWl0?=",
-            "[ 201105191633 ] =?UTF-8?B?IERyZWltb25hdHNmcmlzdCBmw7xyIFZlc" + \
-                "nBmbGVndW5nc21laHJhdWZ3ZW5kdW4=?= =?UTF-8?B?Z2VuIGVpbmVz" + \
-                "IFNlZW1hbm5z?=",
-            "'=?KOI8-R?B?W1JFUS0wMDI1NDEtNDc5NzddIO/h7yAi89TSz8rGwdLGz9Ii" + \
-                "IDs=?=\r\n\t=?KOI8-R?B?Ry43MjkgKDEwKQ==?=",
-            "[ 201101251025 ] ELStAM;=?UTF-8?B?IFZlcmbDvGd1bmcgdm9tIDIx?=" + \
-                ". Januar 2011",
-            "=?koi8-r?B?4tnT1NLP19nQz8zOyc3PIMkgzcHMz9rB1NLB1M7P?=",
-            "=?ISO-8859-2?Q?=E9rdekes?=",
-            "\"=?iso-2022-jp?b?GyRCS1xGfCRPQDJFNyRKJGobKEI=?=\"",
-            "=?windows-1252?Q?Earn_your_degree_=97_on_your_time?=\n=?wind" + \
-                "ows-1252?Q?_and_terms?=",
-        ]
-        for subject in test_subjects:
-            decoded = email_utils.encoding.get_decoded_header(subject)
-            print decoded
+        test_subjects = {
+            "[ 201105161048 ] GewSt: Wegfall der Vorläufigkeit":
+                "[ 201105161048 ] GewSt:=?UTF-8?B?IFdlZ2ZhbGwgZGVyIFZ" + \
+                "vcmzDpHVmaWdrZWl0?=",
+            "[ 201105191633 ] Dreimonatsfrist für Verpflegungsmehrauf" + \
+            "wendungen eines Seemanns":
+                "[ 201105191633 ] =?UTF-8?B?IERyZWltb25hdHNmcmlzdCBmw" + \
+                "7xyIFZlcnBmbGVndW5nc21laHJhdWZ3ZW5kdW4=?= =?UTF-8?B?" + \
+                "Z2VuIGVpbmVzIFNlZW1hbm5z?=",
+            "[REQ-002541-47977] ОАО \"Стройфарфор\" ;G.729 (10)":
+                "=?KOI8-R?B?W1JFUS0wMDI1NDEtNDc5NzddIO/h7yAi89TSz8rGw" + \
+                "dLGz9IiIDs=?=\r\n\t=?KOI8-R?B?Ry43MjkgKDEwKQ==?=",
+            "[ 201101251025 ] ELStAM; Verfügung vom 21. Januar 2011":
+                "[ 201101251025 ] ELStAM;=?UTF-8?B?IFZlcmbDvGd1bmcgdm" + \
+                "9tIDIx?=. Januar 2011",
+            "Быстровыполнимо и малозатратно":
+                "=?koi8-r?B?4tnT1NLP19nQz8zOyc3PIMkgzcHMz9rB1NLB1M7P?=",
+            "érdekes":
+                "=?ISO-8859-2?Q?=E9rdekes?=",
+            "\"本日は晴天なり\"":
+                "\"=?iso-2022-jp?b?GyRCS1xGfCRPQDJFNyRKJGobKEI=?=\"",
+            "Earn your degree — on your time and terms":
+                "=?windows-1252?Q?Earn_your_degree_=97_on_your_time?=" + \
+                "\n=?windows-1252?Q?_and_terms?=",
+            "pöstal":
+                "=?foobar?q?p=F6stal?=",
+        }
+        for subject, encoded in test_subjects.iteritems():
+            decoded = email_utils.encoding.get_decoded_email_header(encoded)
+            self.assertEqual(subject, decoded)
 
 
